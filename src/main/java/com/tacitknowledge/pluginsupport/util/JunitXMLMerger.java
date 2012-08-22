9dd8b22457a6317f1a4c54e0013cce5c8bf4e43a
Petric Coroli <pcoroli@tacitknowledge.com>
2012-08-22 16:00:42 +0300

    Adding copyright section

--- src/main/java/com/tacitknowledge/pluginsupport/util/JunitXMLMerger.java
+++ src/main/java/com/tacitknowledge/pluginsupport/util/JunitXMLMerger.java
@@ -1,21 +1,41 @@
+/* Copyright 2012 Tacit Knowledge
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package com.tacitknowledge.pluginsupport.util;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import org.xml.sax.SAXException;
-import org.apache.tools.ant.taskdefs.optional.junit.DOMUtil;
-import org.apache.tools.ant.util.DOMElementWriter;
-
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.*;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
-import java.io.*;
-import java.util.List;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.tools.ant.taskdefs.optional.junit.DOMUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 
 /**
